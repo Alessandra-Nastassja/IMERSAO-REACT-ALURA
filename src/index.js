374e09e362afec602c69566d155b50f7cda5f916
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2020-08-02 20:46:53 -0300

    carrossel: Creating  carrossel

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import './index.css';
 
 import Home from './pages/home';
 import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
