73d2e31c6086a0556b4ee9fa86be3f9ccfd1cac4
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2020-08-02 19:20:27 -0300

    Creating ap component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import Menu from './components/Menu/index';
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Caroussel from './components/Carousel';
+import Footer from './components/Footer'
+
+import dadosIniciais from './data/dados_iniciais.json';
+
+const AppWrapper = styled.div`
+  background: var(--grayDark);
+`;
 
 function App() {
   return (
-    <>
+    <AppWrapper>
       <Menu />
-    </>
+      <BannerMain 
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+      url={dadosIniciais.categorias[0].videos[0].url}
+      videoDescription={"O front"} />
+
+      <Caroussel 
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[0]} />
+
+      <Footer />
+
+    </AppWrapper>
   );
 }
 
